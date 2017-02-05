0bfc4bc5559de7d7c9912b937432e75bb4ea51ba
SECT <info@sectwebstudio.com>
2017-02-06 00:36:29 +0900

    Fix a JS bug on Option-Page

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,9 +1,9 @@
 jQuery(function(){
-	jQuery("#cfs_lfq_posttype").select2({
+	jQuery("#acf_rfq_posttype").select2({
 		placeholder: "Select a post type",
 		allowClear : true
 	});
-	jQuery("#cfs_lfq_taxonomy").select2({
+	jQuery("#acf_rfq_taxonomy").select2({
 		placeholder: "Select a taxonomy",
 		allowClear : true
 	});
