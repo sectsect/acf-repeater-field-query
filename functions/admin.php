8bbac790cba7e0113ef712d8990229a1df762eac
SECT <info@sectwebstudio.com>
2017-11-16 00:13:18 +0900

    Fix PHP Notice for has_cap

--- functions/admin.php
+++ functions/admin.php
@@ -5,7 +5,7 @@
 add_action('admin_menu', 'acf_rfq_menu');
 function acf_rfq_menu()
 {
-    $page_hook_suffix = add_options_page('ACF Repeater Field Query', 'ACF Repeater Field Query', 8, 'acf_rfq_menu', 'acf_rfq_options_page');
+    $page_hook_suffix = add_options_page('ACF Repeater Field Query', 'ACF Repeater Field Query', 'manage_options', 'acf_rfq_menu', 'acf_rfq_options_page');
     add_action('admin_print_styles-' . $page_hook_suffix, 'acf_rfq_admin_styles');
     add_action('admin_print_scripts-' . $page_hook_suffix, 'acf_rfq_admin_scripts');    // @ https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/wp_enqueue_script#.E3.83.97.E3.83.A9.E3.82.B0.E3.82.A4.E3.83.B3.E7.AE.A1.E7.90.86.E7.94.BB.E9.9D.A2.E3.81.AE.E3.81.BF.E3.81.A7.E3.82.B9.E3.82.AF.E3.83.AA.E3.83.97.E3.83.88.E3.82.92.E3.83.AA.E3.83.B3.E3.82.AF.E3.81.99.E3.82.8B
     add_action('admin_init', 'register_acf_rfq_settings');
